c9e0eddfccd834bac6903e92f71a792e8301c8d1
karajesh <138932079+karajesh@users.noreply.github.com>
2023-07-14 17:22:32 -0400

    simple

---
+++ Week01/simpleRegression.R
@@ -0,0 +1,15 @@
+# Load the required packages
+library(readr)
+
+# Read the iris data from a CSV file
+iris_df <- read_csv("iris.csv", col_names = c("Petal.Length", "Petal.Width", "Sepal.Length", "Sepal.Width", "Species"))
+
+
+# Make a model to relate sepal.length to petal.length
+model = lm (Sepal.Length ~ Petal.Length, data = iris_df)
+
+plot(iris_df$Petal.Length, iris_df$Sepal.Length)
+abline(model)
+
+par(mfrow=  c(2,2))
+plot(model)
